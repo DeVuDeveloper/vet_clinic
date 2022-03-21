ae5fea0b6aef73d4ca940daf8b26c938f15a1707
dejan <drvu47@gmail.com>
2022-03-21 15:50:35 +0100

    transactions and queries

--- queries.sql
+++ queries.sql
@@ -6,3 +6,43 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered='true';
 SELECT * FROM animals WHERE NOT name='Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+SELECT * FROM animals;
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+COMMIT; 
+SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+SELECT * FROM animals;
+SAVEPOINT deleted_ditto;
+UPDATE animals SET weight_kg = -1 * weight_kg;
+SELECT * FROM animals;
+ROLLBACK TO deleted_ditto;
+SELECT * FROM animals;
+UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
+--above Update will not works, explanations in PR
+COMMIT;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+
+
+
+
+
+
+
+
+
+
+
