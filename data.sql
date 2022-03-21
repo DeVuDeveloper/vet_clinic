07bbeb599c78b780ebe29c904b088d4f02094467
dejan <drvu47@gmail.com>
2022-03-21 17:32:10 +0100

    requested changes

--- data.sql
+++ data.sql
@@ -41,10 +41,10 @@ INSERT INTO animals (
     weight_kg
   )
 VALUES
-      ('Charmander', '02-08-2020', '0', 'false', '11'),
-	    ('Plantmon', '11-15-2021', '2', 'true', '5.7'),
-	    ('Squirtle', '04-02-1993', '3', 'false', '12.13'),
-	    ('Angemon', '06-12-2005', '1', 'true', '45'),
-	    ('Boarmon', '06-07-2005', '7', 'true', '20.4'),
-      ('Blossom', '10-13-1998', '3', 'true', '17'),
-      ('Ditto', '05-14-2022', '4', 'true', '22');
+      ('Charmander', '02-08-2020', 0, 'false', -11),
+	    ('Plantmon', '11-15-2021', 2, 'true', -5.7),
+	    ('Squirtle', '04-02-1993', 3, 'false', -12.13),
+	    ('Angemon', '06-12-2005', 1, 'true', -45),
+	    ('Boarmon', '06-07-2005', 7, 'true', 20.4),
+      ('Blossom', '10-13-1998', 3, 'true', 17),
+      ('Ditto', '05-14-2022', 4, 'true', 22);
