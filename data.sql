51ffde2d925543f7b3c4e70d527c07e5abab0688
dejan <drvu47@gmail.com>
2022-03-21 09:08:05 +0100

    data.sql and queries.sql files completed

--- data.sql
+++ data.sql
@@ -1,1 +1,35 @@
-sql
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Agumon', '02/03/2020', 0, TRUE, 10.23);
+
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Gabumon', '11/15/2018', 2, true, 8);
+
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Pikachu', '01/07/2021', 1, false, 15.04);
+
+INSERT INTO animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES ('Devimon', '05/12/2017', 5, true, 11);
