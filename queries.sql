8c91f55cbb899d7c9f93393e6b6b47457f39f591
dejan <drvu47@gmail.com>
2022-03-22 10:27:50 +0100

    half queries done

--- queries.sql
+++ queries.sql
@@ -52,3 +52,19 @@ SELECT * FROM animals JOIN owners ON animals.owner_id = owners.id
 WHERE animals.escape_attempts = 0 AND owners.id = 5;
 SELECT owners.full_name, COUNT(*) AS count FROM owners JOIN animals ON animals.owner_id=owners.id
 GROUP BY owners.id ORDER BY count DESC limit 1;
+
+SELECT animals.name, vets.name, visits.date_of_visit FROM vets
+JOIN visits ON vets.id = visits.vet_id
+JOIN animals ON animals.id = visits.animal_id
+WHERE vets.name = 'William Tatcher'
+ORDER BY visits.date_of_visit DESC LIMIT 1;
+SELECT COUNT(animal_id) FROM visits WHERE vet_id = 3;
+SELECT specializations.species_id, vets.name FROM vets FULL JOIN specializations ON specializations.vet_id=vets.id;
+SELECT animals.name, visits.date_of_visit FROM animals JOIN visits ON visits.animal_id = animals.id
+JOIN vets ON vets.id = visits.vet_id
+WHERE vets.id =3  AND visits.date_of_visit >= '2020-04-01' AND visits.date_of_visit <= '2020-08-30';
+SELECT name, COUNT(*) AS count_of_visits FROM animals JOIN visits ON animals.id=visits.animal_id
+GROUP BY name ORDER BY count_of_visits DESC LIMIT 1;
+
+
+
